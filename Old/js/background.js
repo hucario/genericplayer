c8d2c4e5a8904aa87c62733b656e8172147d0fb2
hucario <h.spam@protonmail.com>
2020-11-05 17:32:25 -0800

    hey it would be really funny if I wiped out my entire codebase and started new haha unless? :flushed:

--- Old/js/background.js
+++ Old/js/background.js
@@ -4,18 +4,15 @@ function updateRating(songId, newRating) { // placeholder function for now
 }
 // @ts-ignore
 function settings(key,value) {
-	console.log(key, value)
 	if (typeof value === "undefined") {
 		return pog[key];
 	}
-	console.log(pog);
 	pog[key] = value;
-	console.log(pog)
 	return value;
 }
 let pog = {
 	historyListView: true,
-	activeExtension: new SampleExtension(),
+	activeExtension: new PandoraExtension(),
 	loggedIn: true,
 	inDev: true
 }
